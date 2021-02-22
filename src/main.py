eeee2dd148a486be322d81a829a245f34dad3eab
ginop-1 <enrico.covili@fermi.mo.it>
2021-02-22 12:55:48 +0100

    Total rewrite using class

--- src/main.py
+++ src/main.py
@@ -1,97 +1,145 @@
-import  tkinter        as      tk
-import  tkinter.ttk    as      ttk
-from    ttkwidgets     import  TickScale
-from    constant       import *
-from    random         import  randint, seed, shuffle
-from    time           import  time
-import  algo
-
-# function
-array_values= []
-
-def Generate():
-    global array_values
-    array_values = []
-    min_value = int(minEntry.get())
-    max_value = int(maxEntry.get())
-    size = int(sizeEntry.get())
-    for i in range(size):
-        array_values.append(int((max_value/size)*(i+1)))
-    shuffle(array_values)
-    Draw(array_values, [rectBlue for x in range(len(array_values))])
-
-def Draw(arr_data, color):
-    drawArea.delete("all")
-    total_height = MAX_HEIGHT-100
-    rect_width = MAX_WIDTH / (len(arr_data)+1)
-    free_space = 20
-    new_arr = []
-    for i in arr_data:
-        new_arr.append( i / max(arr_data))
-    for i, height in enumerate(new_arr):
-        x_left_top      = i * rect_width        + free_space
-        y_left_top      = total_height - height * (total_height-20)
-        x_right_bottom  = (i+1) * rect_width    + free_space
-        y_right_bottom  = total_height
-        drawArea.create_rectangle(x_left_top, y_left_top, x_right_bottom, y_right_bottom, fill=color[i])
-        if int(sizeEntry.get()) < 30:
-            drawArea.create_text(x_left_top+2, y_left_top, anchor='sw', text=str(arr_data[i]))
-    window_root.update_idletasks()
-
-def Start():
-    global array_values
-    if (algorithm_menu.get() == 'SELECTION'):
-        algo.selection_sort(array_values, Draw, speedScale.get())
-    elif (algorithm_menu.get() == 'BUBBLE'):
-        algo.bubble_sort(array_values, Draw, speedScale.get())
-    elif (algorithm_menu.get() == 'INSERTION'):
-        algo.insertion_sort(array_values, Draw, speedScale.get())
-
-# creating window_root
-window_root = tk.Tk()
-window_root.title("Array sorter")
-window_root.maxsize(MAX_WIDTH, MAX_HEIGHT)
-
-# custom style
-style = ttk.Style(window_root)
-window_root.tk.call('source', 'Azure-ttk-theme/azure dark/azure_dark.tcl')
-style.theme_use('azure_dark')
-
-# stringVar
-select_alg_menu = tk.StringVar()
-
-# UI structure
-top_menu = tk.Frame(window_root, width=MAX_WIDTH, height=MAX_HEIGHT/4, bg=grey)
-top_menu.grid(row=0, column=0, pady=4)
-
-drawArea = tk.Canvas(window_root, width=MAX_WIDTH, height=MAX_HEIGHT, bg=lightBlue)
-drawArea.grid(row=1, column=0)
-
-# first ROW
-tk.Label(top_menu, text="Algorithm: ", bg=grey).grid(row=0, column=0, padx=5, pady=5, sticky='')
-algorithm_menu = ttk.Combobox(top_menu, textvariable=select_alg_menu, values=['SELECTION', 'BUBBLE', 'INSERTION'])
-algorithm_menu.grid(row=0, column=1, padx=15, pady=5)
-algorithm_menu.current(0)
-
-tk.Label(top_menu, text="Speed [s]: ", bg=grey).grid(row=0, column=2, padx=15, pady=5, sticky='SW')
-speedScale = TickScale(top_menu, from_=0.01, to=2, digits=2, orient='horizontal')
-speedScale.grid(row=0, column=3, columnspan=3, padx=15, pady=0, sticky='W')
-
-# second ROW
-tk.Label(top_menu, text="Size: ", bg=grey).grid(row=1, column=0, padx=15, pady=3, sticky='SW')
-sizeEntry = TickScale(top_menu, from_=1, to=50, digits=0, orient='horizontal')
-sizeEntry.grid(row=1, column=1, padx=15, pady=0, sticky='W')
+import tkinter as tk
+import tkinter.ttk as ttk
+from ttkwidgets import TickScale
+from constant import *
+from random import randint, seed, shuffle
+from time import time, sleep
 
 
-tk.Label(top_menu, text="Min value: ", bg=grey).grid(row=1, column=2, padx=15, pady=3, sticky='SW')
-minEntry = TickScale(top_menu, from_=1, to=100, digits=0, orient='horizontal')
-minEntry.grid(row=1, column=3, padx=15, pady=3, sticky='W')
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.data = []
+        self.select_alg_menu = tk.StringVar()
+        self.title("Array sorter")
+        self.maxsize(MAX_WIDTH, MAX_HEIGHT)
 
-tk.Label(top_menu, text="Max Value: ", bg=grey).grid(row=1, column=4, padx=15, pady=3, sticky='SW')
-maxEntry = TickScale(top_menu, from_=1, to=100, digits=0, orient='horizontal')
-maxEntry.grid(row=1, column=5, padx=15, pady=3, sticky='W')
+        self.style = ttk.Style(self)
+        self.tk.call(
+            'source', 'Azure-ttk-theme/azure dark/azure_dark.tcl')
+        self.style.theme_use('azure_dark')
 
-ttk.Button(top_menu, text="Generate", command=Generate, style='Accentbutton').grid(row=1, column=6, padx=15)
-ttk.Button(top_menu, text="Start", command=Start, style='Accentbutton').grid(row=0, column=6, padx=15)
+        self.top_menu = tk.Frame(self, width=MAX_WIDTH,
+                                 height=MAX_HEIGHT/4, bg=grey)
 
-window_root.mainloop()
+        self.top_menu.grid(row=0, column=0, pady=4)
+
+        self.drawArea = tk.Canvas(self, width=MAX_WIDTH,
+                                  height=MAX_HEIGHT, bg=lightBlue)
+        self.drawArea.grid(row=1, column=0)
+
+        # first ROW
+        tk.Label(self.top_menu, text="Algorithm: ", bg=grey).grid(
+            row=0, column=0, padx=5, pady=5, sticky='')
+        self.algorithm_menu = ttk.Combobox(
+            self.top_menu, textvariable=self.select_alg_menu, values=[
+                'SELECTION', 'BUBBLE', 'INSERTION'])
+        self.algorithm_menu.grid(row=0, column=1, padx=15, pady=5)
+        self.algorithm_menu.current([0])
+
+        tk.Label(self.top_menu, text="Speed [s]: ", bg=grey).grid(
+            row=0, column=2, padx=15, pady=5, sticky='SW')
+        self.speedScale = TickScale(self.top_menu, from_=0.01, to=2,
+                                    digits=2, orient='horizontal')
+        self.speedScale.grid(row=0, column=3, columnspan=3,
+                             padx=15, pady=0, sticky='W')
+
+        # second ROW
+        tk.Label(self.top_menu, text="Size: ", bg=grey).grid(
+            row=1, column=0, padx=15, pady=3, sticky='SW')
+        self.sizeEntry = TickScale(self.top_menu, from_=1, to=50,
+                                   digits=0, orient='horizontal')
+        self.sizeEntry.grid(row=1, column=1, padx=15, pady=0, sticky='W')
+
+        tk.Label(self.top_menu, text="Min value: ", bg=grey).grid(
+            row=1, column=2, padx=15, pady=3, sticky='SW')
+        self.minEntry = TickScale(self.top_menu, from_=1, to=100,
+                                  digits=0, orient='horizontal')
+        self.minEntry.grid(row=1, column=3, padx=15, pady=3, sticky='W')
+
+        tk.Label(self.top_menu, text="Max Value: ", bg=grey).grid(
+            row=1, column=4, padx=15, pady=3, sticky='SW')
+        self.maxEntry = TickScale(self.top_menu, from_=1, to=100,
+                                  digits=0, orient='horizontal')
+        self.maxEntry.grid(row=1, column=5, padx=15, pady=3, sticky='W')
+
+        ttk.Button(self.top_menu, text="Generate",
+                   command=lambda: self.Generate(),
+                   style='Accentbutton').grid(row=1, column=6, padx=15)
+        ttk.Button(self.top_menu, text="Start", command=lambda: self.Start(
+        ), style='Accentbutton').grid(row=0, column=6, padx=15)
+
+    def Generate(self):
+        self.data.clear()
+        min_value = int(self.minEntry.get())
+        max_value = int(self.maxEntry.get())
+        size = int(self.sizeEntry.get())
+        for i in range(size):
+            self.data.append(int((max_value/size)*(i+1)))
+        shuffle(self.data)
+        self.Draw([rectBlue for x in range(len(self.data))])
+
+    def Draw(self, color=list):
+        self.drawArea.delete("all")
+        total_height = MAX_HEIGHT-100
+        rect_width = MAX_WIDTH / (len(self.data)+1)
+        free_space = 20
+        new_arr = []
+        for i in self.data:
+            new_arr.append(i / max(self.data))
+        for i, height in enumerate(new_arr):
+            x_left_top = i * rect_width + free_space
+            y_left_top = total_height - height * (total_height-20)
+            x_right_bottom = (i+1) * rect_width + free_space
+            y_right_bottom = total_height
+            self.drawArea.create_rectangle(
+                x_left_top, y_left_top, x_right_bottom,
+                y_right_bottom, fill=color[i])
+            if int(self.sizeEntry.get()) < 30:
+                self.drawArea.create_text(
+                    x_left_top+2, y_left_top,
+                    anchor='sw', text=str(self.data[i]))
+        self.update_idletasks()
+
+    def Start(self):
+        if (self.algorithm_menu.get() == 'SELECTION'):
+            self.selection_sort()
+        elif (self.algorithm_menu.get() == 'BUBBLE'):
+            self.bubble_sort()
+        elif (self.algorithm_menu.get() == 'INSERTION'):
+            self.insertion_sort()
+
+    def selection_sort(self):
+        for i in range(len(self.data)):
+            for j in range(len(self.data)):
+                if self.data[i] < self.data[j]:
+                    self.data[i], self.data[j] = self.data[j], self.data[i]
+                    self.Draw([lightGreen if x == i or x ==
+                               j else rectBlue for x in range(len(self.data))])
+                    sleep(self.speedScale.get())
+        self.Draw([lightGreen for x in range(len(self.data))])
+
+    def bubble_sort(self):
+        for i in range(len(self.data)):
+            for j in range(len(self.data)-1):
+                if self.data[j] > self.data[j+1]:
+                    self.data[j], self.data[j+1] = self.data[j+1], self.data[j]
+                    self.Draw([lightGreen if x == j or x == j +
+                               1 else rectBlue for x in range(len(self.data))])
+                    sleep(self.speedScale.get())
+        self.Draw([lightGreen for x in range(len(self.data))])
+
+    def insertion_sort(self):
+        for i in range(len(self.data)):
+            for j in range(i+1, len(self.data)):
+                if self.data[j] < self.data[i]:
+                    self.data[j], self.data[i] = self.data[i], self.data[j]
+                    self.Draw([lightGreen if x == j or x ==
+                               i else rectBlue for x in range(len(self.data))])
+                    sleep(self.speedScale.get())
+        self.Draw([lightGreen for x in range(len(self.data))])
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
